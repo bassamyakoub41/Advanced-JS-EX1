f4986831c5cc4c032e52fbd9c007ec7b8e82f071
Bassam Yakoub <155852055+bassamyakoub41@users.noreply.github.com>
2024-02-09 17:44:11 +0100

    Add files via upload

---
+++ Exr1.js
@@ -0,0 +1,63 @@
+// EX 1 
+
+// let user = {
+//     name: "John",
+//     years: 30
+// }
+// let {name , years ,isAdmen=false}= user
+
+// console.log(name ,years ,isAdmen)
+
+// ex4
+
+// user={};
+// user.name='John';
+// user.surename='Smith';
+// user.name='Pete';
+// delete user.name;
+
+// ex5
+// const user = {
+//     name: "John"
+//   }
+// user.name='pete'
+//   console.log(user)
+
+// ex 6 
+// let salaries = {
+//     Fred: 100,
+//     Ted: 160,
+//     Ghaith: 130
+// }
+// if(salaries!=null && Object.keys(salaries).length!=0){
+//     sum= salaries.Fred + salaries.Ted+salaries.Ghaith
+// }else{
+//     sum=0
+// }
+// console.log(sum)
+
+// ex7
+
+// if (a + b < 4) {
+//     result = 'Below';
+//   } else {
+//     result = 'Over';
+//   }
+
+//   (a+b<4)?'below' : "Over";
+
+// ex8
+
+// let message;
+
+// if (login == 'Employee') {
+//   message = 'Hello';
+// } else if (login == 'Director') {
+//   message = 'Greetings';
+// } else if (login == '') {
+//   message = 'No login';
+// } else {
+//   message = '';
+// }
+
+// let new=(login=='employee')? 'Hello' : 'Director'? 'Greetings' : ''? 'No login' : '';
